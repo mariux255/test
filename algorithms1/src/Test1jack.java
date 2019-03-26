ea148ded30076f0ea84df7f2759ae7458bdd45f1
mariux255 <44610785+mariux255@users.noreply.github.com>
2019-03-26 16:00:51 +0100

    comment update

--- algorithms1/src/Test1jack.java
+++ algorithms1/src/Test1jack.java
@@ -4,5 +4,6 @@ public class Test1jack {
         //Does this work,Jack?
         System.out.println("fuck you marius");
         System.out.println("Jack sucks a massive pee-pee");
+        //new update
     }
 }

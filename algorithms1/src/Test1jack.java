f5b6d5e8165be1d5d49a4c81684b8bda81784dcf
mariux255 <44610785+mariux255@users.noreply.github.com>
2019-03-26 15:48:11 +0100

    Marius commit

--- algorithms1/src/Test1jack.java
+++ algorithms1/src/Test1jack.java
@@ -2,5 +2,6 @@ public class Test1jack {
     Test1jack(){
         System.out.print("wuut");
         System.out.println("fuck you marius");
+        System.out.println("Jack sucks a massive pee-pee");
     }
 }

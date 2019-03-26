035682b00e2d3097d1e99f49f6d8c9f113f451bb
mariux255 <44610785+mariux255@users.noreply.github.com>
2019-03-26 15:50:09 +0100

    edit code

--- algorithms1/src/Test1jack.java
+++ algorithms1/src/Test1jack.java
@@ -1,6 +1,7 @@
 public class Test1jack {
     Test1jack(){
-        System.out.print("wuut");
+        System.out.println("Shrek");
+        //Does this work,Jack?
         System.out.println("fuck you marius");
         System.out.println("Jack sucks a massive pee-pee");
     }
